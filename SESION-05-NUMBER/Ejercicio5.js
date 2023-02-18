b81e5330deaf63e0d439a5f7791c6539ba3bed5e
DiegoScott <99587513+2003scott@users.noreply.github.com>
2023-02-17 20:33:49 -0500

    Add files via upload

---
+++ SESION-05-NUMBER/Ejercicio5.js
@@ -0,0 +1,12 @@
+var altura = 1.75;
+console.log(parseInt(altura));
+console.log(parseFloat(altura));
+
+var peso = 71.200;
+console.log(parseFloat(peso));
+
+console.log(Math.ceil(altura));
+console.log(Math.floor(peso));
+
+let max_valor_js = Number.MAX_VALUE;
+console.log(max_valor_js+1)
---
+++ SESION-05-NUMBER/declaraciones-de-variable-numericas.js
@@ -0,0 +1,13 @@
+//SESION 05 - NUMBERS
+//DECLARACION DE VARIABLES NUMERICAS (ENTEROS Y CON DECIMALES)
+let a=5;
+console.log(a);
+
+let b=0.1;
+console.log(b);
+
+//Precicion
+let c=0.2;
+console.log(b + c);
+//Pequeño truco para obtener valores "reales"
+console.log(Math.round((0.1+0.2)*100)/100);
