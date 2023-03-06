5dc43974583f61f3c564bf0343c3bf724b334e37
DiegoScott <99587513+2003scott@users.noreply.github.com>
2023-03-05 20:27:31 -0500

    Add files via upload

---
+++ SESION-13-Eslint/eslint-npm/index.js
@@ -0,0 +1,21 @@
+//Este es el archivo que vamos a utilizar
+
+const nombre = "Diego"
+
+//En esta linea quiero que me dessactives las comillas simples
+/*eslint-disable*/
+const persona2 = 'Diego'
+/*eslint-enable*/
+
+const nuevoString = 'Esto es un string'; //eslint-disable-line
+
+/* eslint-disable-next-line indent */
+  const string2 = "Mas String"
+
+console.log("Hola")
+
+const persona3 = "Maria"
+
+const nombre2 = "Julia"
+
+console.log(4)
---
+++ SESION-13-Eslint/index.js
@@ -0,0 +1,9 @@
+var nombre = "Scott"
+
+var nombre2 = 'Diego';
+
+var objeto = {
+    nombre : "Circulo",
+    radio: 2,
+    nombre : "Rectangulo",
+}
